fd7b3797e84c5d502353d950510db232637d4045
Nika593 <159131871+Nika593@users.noreply.github.com>
2024-04-09 17:51:21 +0600

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event21_Act1(runtime, localVars)
+	{
+		if (runtime.objects.Stone.getPickedInstances()[0].instVars["value"] == runtime.objects.Stone.getPickedInstances()[1].instVars["counterValue"]) {
+			localVars.result = 1
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
